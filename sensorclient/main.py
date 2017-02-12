3d778684a14d6b272c81c7d249ed086386720be7
Lars Kellogg-Stedman <lars@redhat.com>
2017-02-12 00:12:32 -0500

    use outlined_cube instead of colored_cube from vispy examples

--- sensorclient/main.py
+++ sensorclient/main.py
@@ -1,97 +1,7 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Copyright (c) 2015, Vispy Development Team. All Rights Reserved.
-# Distributed under the (new) BSD License. See LICENSE.txt for more info.
-# -----------------------------------------------------------------------------
-# Author: Nicolas P .Rougier
-# Date:   04/03/2014
-# -----------------------------------------------------------------------------
-
+from vispy import gloo, app
 import argparse
-import numpy as np
-
-from vispy import app, gloo
-from vispy.gloo import Program, VertexBuffer, IndexBuffer
-from vispy.util.transforms import perspective, translate, rotate
-from vispy.geometry import create_cube
-
 import sensorclient
-
-vertex = """
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-attribute vec3 position;
-attribute vec2 texcoord;
-attribute vec3 normal;
-attribute vec4 color;
-
-varying vec4 v_color;
-void main()
-{
-    v_color = color;
-    gl_Position = projection * view * model * vec4(position,1.0);
-}
-"""
-
-fragment = """
-varying vec4 v_color;
-void main()
-{
-    gl_FragColor = v_color;
-}
-"""
-
-
-class Canvas(app.Canvas):
-    def __init__(self, sensor=None):
-        app.Canvas.__init__(self, size=(512, 512), title='Colored cube',
-                            keys='interactive')
-
-        # Build cube data
-        V, I, _ = create_cube()
-        vertices = VertexBuffer(V)
-        self.indices = IndexBuffer(I)
-
-        # Build program
-        self.program = Program(vertex, fragment)
-        self.program.bind(vertices)
-
-        # Build view, model, projection & normal
-        view = translate((0, 0, -5))
-        model = np.eye(4, dtype=np.float32)
-        self.program['model'] = model
-        self.program['view'] = view
-        gloo.set_state(clear_color=(0.30, 0.30, 0.35, 1.00), depth_test=True)
-
-        self.activate_zoom()
-
-        self.sensor = sensor
-        self.timer = app.Timer('auto', self.on_timer, start=True)
-
-        self.show()
-
-    def on_draw(self, event):
-        gloo.clear(color=True, depth=True)
-        self.program.draw('triangles', self.indices)
-
-    def on_resize(self, event):
-        self.activate_zoom()
-
-    def activate_zoom(self):
-        gloo.set_viewport(0, 0, *self.physical_size)
-        projection = perspective(45.0, self.size[0] / float(self.size[1]),
-                                 2.0, 10.0)
-        self.program['projection'] = projection
-
-    def on_timer(self, event):
-        data = next(self.sensor)
-        self.program['model'] = np.dot(
-            rotate(data[0], (1, 0, 0)),
-            rotate(data[1], (0, 0, 1)),
-        )
-        self.update()
+import outlined_cube
 
 def parse_args():
     p = argparse.ArgumentParser()
@@ -105,6 +15,5 @@ def main():
     host, port = args.sensor.split(':')
     sensor = (host, int(port))
     s = sensorclient.SocketClient(sensor)
-#    c1 = Canvas(sensor=s, valsfrom='pos')
-    c2 = Canvas(sensor=s)
+    c = outlined_cube.Canvas(sensor=s)
     app.run()
